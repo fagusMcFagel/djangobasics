543be4b058793434bde3879adab0ab91499cf5e9
forner <forner@BUERO-09W7.goebel.local>
2017-07-19 12:03:57 +0200

    Added specific error recognition an corresponding error messages

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -7,14 +7,33 @@ from books.models import Book
 #def search_form(request):
 #    return render(request, 'books/search_form.html')
 
+#===============================================================================
+# earlier version with error as boolean -> unspecific error messages
+# def search(request):
+#     error=False
+#     if 'q' in request.GET:
+#         q = request.GET['q']
+#         if not q:
+#             error=True
+#         elif len(q)>20:
+#             error=True
+#         else:
+#             books = Book.objects.filter(title__icontains=q)
+#             return render(request, 'books/search_results.html', 
+#                           {'books': books, 'query': q})
+#     return render(request, 'books/search_form.html', {'errors': errors})
+#===============================================================================
+
 def search(request):
-    error = False
+    errors = []
     if 'q' in request.GET:
         q = request.GET['q']
-        if q:
+        if not q:
+            errors.append('Enter a search term.')
+        elif len(q)>20:
+            errors.append('Please enter at most 20 characters.')
+        else:
             books = Book.objects.filter(title__icontains=q)
             return render(request, 'books/search_results.html', 
                           {'books': books, 'query': q})
-        else:
-            error=True
-    return render(request, 'books/search_form.html', {'error': error})
+    return render(request, 'books/search_form.html', {'errors': errors})
