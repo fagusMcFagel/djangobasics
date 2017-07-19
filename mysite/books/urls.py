4f5256246c30dd4671b3908193cefb78172fc21b
forner <forner@BUERO-09W7.goebel.local>
2017-07-19 11:47:10 +0200

    Updated view for search, commented code for search_form in view.py & urls.py

--- mysite/books/urls.py
+++ mysite/books/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 from books import views
 
 urlpatterns = [
-    url(r'^search-form/$', views.search_form),
     url(r'^search/$', views.search)
+    ## NOT NEEDED DUE TO UPDATED VIEW search(request) ##
+    #url(r'^search-form/$', views.search_form),
 ]
--- mysite/books/views.py
+++ mysite/books/views.py
@@ -3,14 +3,18 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from books.models import Book
 
-def search_form(request):
-    return render(request, 'books/search_form.html')
+## NOT NEEDED DUE TO UPDATED VIEW search(request) ##
+#def search_form(request):
+#    return render(request, 'books/search_form.html')
 
 def search(request):
+    error = False
     if 'q' in request.GET:
         q = request.GET['q']
-        books = Book.objects.filter(title__icontains=q)
-        return render(request, 'books/search_results.html', 
-                      {'books': books, 'query': q})
-    else:
-        return HttpResponse('Please enter a search term')
+        if q:
+            books = Book.objects.filter(title__icontains=q)
+            return render(request, 'books/search_results.html', 
+                          {'books': books, 'query': q})
+        else:
+            error=True
+    return render(request, 'books/search_form.html', {'error': error})
