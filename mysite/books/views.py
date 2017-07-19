7d198f285cf8e522cd1925d7aa7a137f33ff16dc
forner <forner@BUERO-09W7.goebel.local>
2017-07-19 11:35:46 +0200

    Added search form, functionality and a template for results

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,3 +1,16 @@
+# \books\views.py
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from books.models import Book
 
-# Create your views here.
+def search_form(request):
+    return render(request, 'books/search_form.html')
+
+def search(request):
+    if 'q' in request.GET:
+        q = request.GET['q']
+        books = Book.objects.filter(title__icontains=q)
+        return render(request, 'books/search_results.html', 
+                      {'books': books, 'query': q})
+    else:
+        return HttpResponse('Please enter a search term')
